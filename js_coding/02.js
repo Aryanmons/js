d8e438d0c9620cc4ae8a9376f4a7f52283914a20
Aryan <126823359+Aryanmons@users.noreply.github.com>
2024-08-08 16:43:09 +0000

    conatainer config done

---
+++ js_coding/02.js
@@ -0,0 +1,18 @@
+"use strict"; //TREAT ALL JS CODE AS NEWER VERSION
+//alert(3 + 3) WE ARE USING NODEJS NOT BROWSER
+
+let name = 'Aryan';
+let age = 18;
+//number => 2 to pwer 53
+//bignit
+//string =>"
+//boolean => true/false
+//null => standalone value
+// undefined => not assignd value
+//symbol => unique
+
+
+
+// object 
+
+console.log(typeof undefined);
