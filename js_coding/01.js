74aa716b389abc698057a7162fb264153a1044b3
Aryan <126823359+Aryanmons@users.noreply.github.com>
2024-08-08 16:23:51 +0000

    container config done

---
+++ js_coding/01.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accontemail = "aryangoogle@google.com"
+var accountpass = "12345"
+accountcity = "jaipur"
+let accountstate;
+//accountId = 2  // NOT ALOWED
+console.log(accountId);
+
+
+accontemail = "hc@hcmail"
+accountpass = "7639873"
+accountcity = "bengal"
+
+console.log(accountId);
+console.table([accountId, accontemail , accountpass , accountcity,accountstate])
